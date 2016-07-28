59deda9d9b8524069db5580315fc6f55bbc94282
Daniel Reist <djreist@outlook.com>
2016-07-28 19:10:56 -0400

    Troubleshooting xScheduledTask

--- Kiosk/Configuration/Generate_Pull_MOF_Files.ps1
+++ Kiosk/Configuration/Generate_Pull_MOF_Files.ps1
@@ -146,15 +146,15 @@ Configuration McBIC_Kids_Checkin
         xScheduledTask SyncRepository
         {
             TaskName = 'Retrieve Configuration Updates'
-            TaskPath = 'C:\Repos\McBICKids'
+            #TaskPath = 'C:\Repos\McBICKids'
             ActionExecutable = 'C:\Program Files\Git\cmd\git.exe'
-            ActionArguments = ' pull "https://github.com/randomnote1/McBICKids.git"'
+            #ActionArguments = ' pull "https://github.com/randomnote1/McBICKids.git"'
             ScheduleType = 'Minutes'
             RepeatInterval = 15
-            StartTime = '12:00 AM'
-            Ensure = 'Present'
-            Enable = $true
-            DependsOn = '[File]PullFolder'
+            #StartTime = '12:00 AM'
+            #Ensure = 'Present'
+            #Enable = $true
+            #DependsOn = '[File]PullFolder'
         }
     }
 }
