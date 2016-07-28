37eb57a29e1da6247fac096b3492742ff1a7275d
Daniel Reist <djreist@outlook.com>
2016-07-27 22:05:08 -0400

    Updated the manual commands to autmatically set the execution policy

--- Kiosk/Configuration/Initialize_Kiosk.ps1
+++ Kiosk/Configuration/Initialize_Kiosk.ps1
@@ -2,7 +2,7 @@
 # Run these commands manually prior to running this script
 $initializationScript = Join-Path -Path $env:USERPROFILE -ChildPath 'Downloads\Initialize_Kiosk.ps1'
 (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/randomnote1/McBICKids/master/Kiosk/Configuration/Initialize_Kiosk.ps1',$initializationScript)
-Set-ExecutionPolicy RemoteSigned
+'A' | Set-ExecutionPolicy RemoteSigned
 & $initializationScript
 #>
 
