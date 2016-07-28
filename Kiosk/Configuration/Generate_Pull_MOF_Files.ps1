dee5ec7d1451f940647a456962f9f8a688f31023
Daniel Reist <djreist@outlook.com>
2016-07-28 16:15:54 -0400

    Updated the schedule task to perform a pull request against the repository instead of a fetch.

--- Kiosk/Configuration/Generate_Pull_MOF_Files.ps1
+++ Kiosk/Configuration/Generate_Pull_MOF_Files.ps1
@@ -149,7 +149,7 @@ Configuration McBIC_Kids_Checkin
             RepeatInterval = 15
             ScheduleType = 'Minutes'
             TaskName = 'Retrieve Configuration Updates'
-            ActionArguments = ( 'fetch "https://github.com/randomnote1/McBICKids.git"' )
+            ActionArguments = ( 'pull "https://github.com/randomnote1/McBICKids.git"' )
             DependsOn = '[File]PullFolder'
             Enable = $true
             Ensure = 'Present'
