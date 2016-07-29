1cefbe7404a0d8c3fada8cc0032eafdb0e2c2b5e
Daniel Reist <djreist@outlook.com>
2016-07-29 17:02:18 -0400

    Removed the start time of the scheduled task.

--- Kiosk/Configuration/Generate_Pull_MOF_Files.ps1
+++ Kiosk/Configuration/Generate_Pull_MOF_Files.ps1
@@ -152,7 +152,7 @@ Configuration McBIC_Kids_Checkin
             ActionArguments = ' pull "https://github.com/randomnote1/McBICKids.git"'
             ScheduleType = 'Minutes'
             RepeatInterval = 15
-            StartTime = '12:10 AM'
+            #StartTime = '12:10 AM'
             Ensure = 'Present'
             Enable = $true
             DependsOn = '[File]PullFolder'
